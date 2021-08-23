898d66d3a3a6a8e37a1f2bb45fea0e4d18f91fa9
Anil Patel <anil.shayona@gmail.com>
2021-08-23 16:09:46 +0530

    category done

---
+++ api/objects/category.php
@@ -0,0 +1,170 @@
+<?php
+class Category{
+  
+    // database connection and table name
+    private $conn;
+    private $table_name = "categories";
+  
+    // object properties
+    public $id;
+    public $name;
+    public $description;
+    public $created;
+    public $modified;
+  
+    public function __construct($db){
+        $this->conn = $db;
+    }
+  
+    // used by select drop-down list
+    public function readAll(){
+        //select all data
+        $query = "SELECT
+                    id, name, description
+                FROM
+                    " . $this->table_name . "
+                ORDER BY
+                    name";
+  
+        $stmt = $this->conn->prepare( $query );
+        $stmt->execute();
+  
+        return $stmt;
+    }
+
+    // used by select drop-down list
+    public function read(){
+    
+        //select all data
+        $query = "SELECT
+                    id, name, description
+                FROM
+                    " . $this->table_name . "
+                ORDER BY
+                    name";
+    
+        $stmt = $this->conn->prepare( $query );
+        $stmt->execute();
+    
+        return $stmt;
+    }
+
+    public function create()
+    {
+
+        // query to insert record
+        $query = "INSERT INTO
+                " . $this->table_name . "
+            SET
+                name=:name, description=:description, created=:created";
+
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->description = htmlspecialchars(strip_tags($this->description));
+        $this->created = htmlspecialchars(strip_tags($this->created));
+
+        // bind values
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":description", $this->description);
+        $stmt->bindParam(":created", $this->created);
+
+        // execute query
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function update(){
+  
+        // update query
+        $query = "UPDATE
+                    " . $this->table_name . "
+                SET
+                    name = :name,
+                    description = :description,
+                    modified = :modified
+                WHERE
+                    id = :id";
+      
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+      
+        // sanitize
+        $this->name=htmlspecialchars(strip_tags($this->name));
+        $this->description=htmlspecialchars(strip_tags($this->description));
+        $this->modified=htmlspecialchars(strip_tags($this->modified));
+        $this->id=htmlspecialchars(strip_tags($this->id));
+      
+        // bind new values
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':description', $this->description);
+        $stmt->bindParam(':modified', $this->modified);
+        $stmt->bindParam(':id', $this->id);
+      
+        // execute the query
+        if($stmt->execute()){
+            return true;
+        }
+      
+        return false;
+    }
+
+    public function delete(){
+  
+        // delete query
+        $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+      
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+      
+        // sanitize
+        $this->id=htmlspecialchars(strip_tags($this->id));
+      
+        // bind id of record to delete
+        $stmt->bindParam(1, $this->id);
+      
+        // execute query
+        if($stmt->execute()){
+            return true;
+        }
+      
+        return false;
+    }
+
+    public function readOne(){
+  
+        // query to read single record
+        $query = "SELECT
+                    id, name, description, created, modified
+                FROM
+                    " . $this->table_name . "
+                WHERE
+                    id = ?
+                LIMIT
+                    0,1";
+      
+        // prepare query statement
+        $stmt = $this->conn->prepare( $query );
+      
+        // bind id of product to be updated
+        $stmt->bindParam(1, $this->id);
+      
+        // execute query
+        $stmt->execute();
+      
+        // get retrieved row
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+      
+        // set values to object properties
+        $this->name = $row['name'];
+        $this->description = $row['description'];
+        $this->created = $row['created'];
+        
+    }
+}
+?>
